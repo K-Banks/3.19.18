11f1531e1bafdcb17a2a8746f1bd28889d6607f2
Ami Cooper and Kayl Eubanks <ami@>
2018-03-19 11:27:44 -0700

    create basic file, add bmi equation, add if statements to print healthy, underweight or obese

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var height = parseInt(prompt("Enter height in inches"));
+var weight = parseInt(prompt("Enter weight in pounds"));
+
+var heightsquare = ((height*0.0254)*(height*0.0254));
+weight = (weight*0.4535);
+var bmi = (weight/heightsquare);
+
+alert(bmi);
+
+if (bmi < 18){
+  alert("Your BMI is underweight")
+}
+if (bmi > 18, bmi < 25){
+  alert("Your BMI is healthy");
+}
+if (bmi > 25) {
+  alert("Your BMI is obese");
+}
